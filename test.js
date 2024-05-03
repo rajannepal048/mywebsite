f8aa0c05ed59492f5aa42ad9a3e834ded9ca1839
Rajan Nepal <58668946+rajannepal048@users.noreply.github.com>
2024-05-02 20:04:45 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+// JS 
+
+$('.list').SwappingWall({
+ 'time': 2000,
+ 'itemsInRow': 4,
+ 'duration': 400,
+ 'responsive': [
+  {
+   'breakpoint': 991,
+   'itemsInRow': 3,
+  },
+  {
+   'breakpoint': 768,
+   'itemsInRow': 2,
+  }
+ ]
+})
